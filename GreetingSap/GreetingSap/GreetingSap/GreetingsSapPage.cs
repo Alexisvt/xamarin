2353ed3515dde376c8091aafc755cf01a9336b10
alexisvt <alexisvt@gmail.com>
2015-04-29 13:40:21 -0700

    Initial commit

---
+++ GreetingSap/GreetingSap/GreetingSap/GreetingsSapPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GreetingSap
+{
+	public class GreetingsSapPage : ContentPage
+	{
+		public GreetingsSapPage ()
+		{
+            this.Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms SAP",
+                //HorizontalOptions= LayoutOptions.Center,
+                //VerticalOptions= LayoutOptions.Center
+
+                XAlign = TextAlignment.Center,
+                YAlign = TextAlignment.Center
+            };
+
+            //#if __IOS__
+            //    this.Padding = new Thickness
+            //    {
+            //        Top= 20
+            //    };
+            //#endif
+
+            //#if WINDOWS_PHONE
+            //    this.Padding = new Thickness
+            //    {
+            //        Top= 150
+            //    };
+            //#endif
+
+            //this.Padding = Device.OnPlatform<Thickness>(new Thickness(0, 20, 0, 0), new Thickness(0), new Thickness(0,300,0,0));
+
+            //Device.OnPlatform(
+            //    iOS: () => { Padding = new Thickness(0, 40, 20, 0); }, 
+            //    Android: () => { Padding= new Thickness(0,0); },
+            //    WinPhone: () => { Padding = new Thickness(0, 100, 0, 0); }
+            //    );
+
+            //Device.OnPlatform(
+            //    iOS: () => { Ver});
+
+		}
+	}
+}
---
+++ Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            //Label label = new Label 
+            //{
+            //    Text = "Greetings, Xamarin.Forms!"
+            //};
+            //this.Content = label;
+
+            this.Padding = new Thickness
+            {
+                Top = 20
+            };
+
+            this.Content = new Label
+            {
+                Text = "Greetings, Xamarin.Forms!"
+            };
+        }
+    }
+}
