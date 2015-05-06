2934c9c3902f406e5afe73876dce8861d4ab2ccb
alexisvt <alexisvt@gmail.com>
2015-05-06 10:32:35 -0700

    Completing chapter 4 practices

---
+++ BlackCat/BlackCat/BlackCat/BlackCatPage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BlackCat
+{
+    public class BlackCatPage : ContentPage
+    {
+        public BlackCatPage()
+        {
+            StackLayout mainStack = new StackLayout();
+            StackLayout textStack = new StackLayout
+            {
+                Padding = new Thickness(5),
+                Spacing = 10
+            };
+
+            // Get access to the text resources
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+            string resource = "BlackCat.Texts.Book.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resource))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    bool gotTitle = false;
+                    string line;
+
+                    // Read in a line (which is actually a paragraph).
+                    while (null != (line = reader.ReadLine()))
+                    {
+                        Label label = new Label
+                        {
+                            Text = line,
+                            // Black text for ebooks!
+                            TextColor = Color.Black
+                        };
+
+                        if (!gotTitle)
+                        {
+                            // Add first label (the title) to mainStack.
+                            label.HorizontalOptions = LayoutOptions.Center;
+                            label.FontSize = Device.GetNamedSize(NamedSize.Medium, label);
+                            mainStack.Children.Add(label);
+                            gotTitle = true;
+                        }
+                        else
+                        {
+                            // Add subsequent labels to textStack
+                            textStack.Children.Add(label);
+                        }
+                    }
+                }
+            }
+
+            // Put the textStack in a ScrollView with FillandExpand.
+            ScrollView scrollView = new ScrollView
+            {
+                Content = textStack,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Padding = new Thickness(5, 0)
+            };
+
+            // Add the ScrollView as a second child of mainStack
+            mainStack.Children.Add(scrollView);
+
+            //Set page content to mainStack
+            Content = mainStack;
+
+            //white background for ebooks!
+            BackgroundColor = Color.White;
+
+            //Add some iOS padding for the page.
+            Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+        }
+    }
+}
---
+++ SizeBoxView/SizeBoxView/SizeBoxView/SizeBoxViewPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SizeBoxView
+{
+    public class SizeBoxViewPage : ContentPage
+    {
+        public SizeBoxViewPage()
+        {
+            BackgroundColor = Color.Pink;
+
+            Content = new BoxView
+            {
+                Color = Color.Accent,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                WidthRequest = 200,
+                HeightRequest = 100
+            };
+        }
+    }
+}
