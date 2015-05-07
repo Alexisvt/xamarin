3729e17e7337c293a6845f6b483f10569b979100
alexisvt <alexisvt@gmail.com>
2015-05-07 14:17:39 -0700

    finishing chapter 5

---
+++ FontSizes/FontSizes/FontSizes/FontSizesPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FontSizes
+{
+    public class FontSizesPage : ContentPage
+    {
+        public FontSizesPage()
+        {
+            BackgroundColor = Color.White;
+            StackLayout stackLayout = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            NamedSize[] namedSizes = {
+                                         NamedSize.Default, NamedSize.Micro
+                                         , NamedSize.Small, NamedSize.Medium, NamedSize.Large
+                                     };
+
+            foreach (NamedSize namedSize in namedSizes)
+            {
+                double fontSize = Device.GetNamedSize(namedSize, typeof(Label));
+
+                stackLayout.Children.Add(new Label
+                {
+                    Text = String.Format("Named Size = {0} ({1:F2})", namedSize, fontSize),
+                    FontSize = fontSize,
+                    TextColor = Color.Black
+                });
+            }
+
+            double resolution = Device.OnPlatform(160, 160, 240);
+
+            stackLayout.Children.Add(
+                new BoxView
+                {
+                    Color = Color.Accent,
+                    HeightRequest = resolution / 80
+                });
+
+            int[] ptSizes = { 4, 6, 8, 10, 12 };
+
+            foreach (double ptSize in ptSizes)
+            {
+                double fontSize = resolution * ptSize / 72;
+
+                stackLayout.Children.Add(new Label
+                {
+                    Text = String.Format("Point Size = {0} ({1:F2})", ptSize, fontSize),
+                    FontSize = fontSize,
+                    TextColor = Color.Black
+                });
+            }
+
+            Content = stackLayout;
+        }
+    }
+}
---
+++ WhatSize/WhatSize/WhatSize/WhatSizePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace WhatSize
+{
+    public class WhatSizePage : ContentPage
+    {
+        Label label;
+
+        public WhatSizePage()
+        {
+            label = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            
+            Content = label;
+
+            SizeChanged += OnPageSizeChanged;
+        }
+
+        void OnPageSizeChanged(object sender, EventArgs e)
+        {
+            label.Text = String.Format("{0} \u00D7 {1}", Width, Height);
+        }
+    }
+}
