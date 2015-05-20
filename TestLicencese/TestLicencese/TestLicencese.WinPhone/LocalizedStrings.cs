fd79ed69e4ff1f351f903f94ed459bc211ad09d7
alexisvt <alexisvt@gmail.com>
2015-05-20 08:34:08 -0700

    commit

---
+++ TestLicencese/TestLicencese/TestLicencese.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TestLicencese.WinPhone.Resources;
+
+namespace TestLicencese.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
