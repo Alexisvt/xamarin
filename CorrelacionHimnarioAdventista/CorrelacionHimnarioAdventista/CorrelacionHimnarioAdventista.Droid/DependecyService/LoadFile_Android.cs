9a66d06c5382c253388497ecd6c92d1469edb5fc
alexisvt <alexisvt@gmail.com>
2015-07-21 17:11:54 -0700

    adding news

--- CorrelacionHimnarioAdventista/CorrelacionHimnarioAdventista/CorrelacionHimnarioAdventista.Droid/DependecyService/LoadFile_Android.cs
+++ CorrelacionHimnarioAdventista/CorrelacionHimnarioAdventista/CorrelacionHimnarioAdventista.Droid/DependecyService/LoadFile_Android.cs
@@ -1,30 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Xamarin.Forms;
 using CorrelacionHimnarioAdventista.Models.Abstract;
-using CorrelacionHimnarioAdventista.Droid.DependecyService;
+using CorrelacionHimnarioAdventista.Droid;
 using HelperClasses;
 
 [assembly: Dependency(typeof(LoadFile_Android))]
 
-namespace CorrelacionHimnarioAdventista.Droid.DependecyService
+namespace CorrelacionHimnarioAdventista.Droid
 {
     public class LoadFile_Android : ILoadFile
     {
-        public Maybe<String> LoadJson(string fileName)
+        public Maybe<string> LoadJson(string fileName)
         {
             string jsonData;
-            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             var filePath = Path.Combine(documentsPath, fileName);
 
             if (!File.Exists(filePath))
